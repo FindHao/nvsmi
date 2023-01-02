813099de27b2d040a2718aa730607111608aa460
FindHao <yhao24@ncsu.edu>
2023-01-01 20:25:25 -0500

    update define

--- src/nvsmi.cpp
+++ src/nvsmi.cpp
@@ -8,18 +8,27 @@
 using std::make_unique;
 
 GPUInforPtrVector *gpuinfors;
+#ifndef NVML_ERROR_CHECK
+#define NVML_ERROR_CHECK(call)                                                   \
+    {                                                                            \
+        auto status = static_cast<nvmlReturn_t>(call);                           \
+        if (status != NVML_SUCCESS)                                              \
+        {                                                                        \
+            fprintf(stderr,                                                      \
+                    "ERROR: CUDA NVML call \"%s\" in line %d of file %s failed " \
+                    "with "                                                      \
+                    "%s (%d).\n",                                                \
+                    #call,                                                       \
+                    __LINE__,                                                    \
+                    __FILE__,                                                    \
+                    nvmlErrorString(status),                                     \
+                    status);                                                     \
+            nvmlShutdown();                                                      \
+            exit(1);                                                             \
+        }                                                                        \
+    }
+#endif // NVML_ERROR_CHECK
 
-#define NVML_ERROR_CHECK(result)                              \
-    do                                                        \
-    {                                                         \
-        nvmlReturn_t res = result;                            \
-        if (NVML_SUCCESS != res)                              \
-        {                                                     \
-            printf("NVML error: %s\n", nvmlErrorString(res)); \
-            nvmlShutdown();                                   \
-            exit(1);                                          \
-        }                                                     \
-    } while (0)
 
 int main(void)
 {
@@ -41,7 +50,7 @@ int main(void)
         // Get the list of graphics processes running on the GPU
         nvmlProcessInfo_t infos[64];
         unsigned int info_count;
-        NVML_ERROR_CHECK(nvmlDeviceGetGraphicsRunningProcesses_v3(device, &info_count, infos));
+        NVML_ERROR_CHECK(nvmlDeviceGetGraphicsRunningProcesses(device, &info_count, infos));
         // Get the UUID of the GPU
         char uuid[NVML_DEVICE_UUID_BUFFER_SIZE];
         NVML_ERROR_CHECK(nvmlDeviceGetUUID(device, uuid, NVML_DEVICE_UUID_BUFFER_SIZE));
@@ -73,26 +82,26 @@ int main(void)
         // get power usage
         unsigned int power_usage;
         NVML_ERROR_CHECK(nvmlDeviceGetPowerUsage(device, &power_usage));
-        gpuinfor->power_usage = power_usage/1000;
+        gpuinfor->power_usage = power_usage / 1000;
         // get power limit
         unsigned int power_limit;
         NVML_ERROR_CHECK(nvmlDeviceGetPowerManagementLimit(device, &power_limit));
-        gpuinfor->power_limit = power_limit/1000;
+        gpuinfor->power_limit = power_limit / 1000;
         // get power management mode
         nvmlEnableState_t power_management_mode;
         NVML_ERROR_CHECK(nvmlDeviceGetPowerManagementMode(device, &power_management_mode));
         // get memory usage
-        nvmlMemory_t memory;
-        nvmlDeviceGetMemoryInfo(device, &memory);
-        gpuinfor->memory_total = memory.total / 1024 / 1024;
+        nvmlMemory_v2_t memory;
+        nvmlDeviceGetMemoryInfo_v2(device, &memory);
+        gpuinfor->memory_total = memory.total /  1024 / 1024;
         gpuinfor->memory_used = memory.used / 1024 / 1024;
         // get utilization
         nvmlUtilization_t utilization;
-        nvmlDeviceGetUtilizationRates(device, &utilization);
+        NVML_ERROR_CHECK(nvmlDeviceGetUtilizationRates(device, &utilization));
         gpuinfor->utilization.gpu = utilization.gpu;
         gpuinfor->utilization.memory = utilization.memory;
         // get SM version
-        nvmlDeviceGetCudaComputeCapability(device, &gpuinfor->sm_version.major, &gpuinfor->sm_version.minor);
+        NVML_ERROR_CHECK(nvmlDeviceGetCudaComputeCapability(device, &gpuinfor->sm_version.major, &gpuinfor->sm_version.minor));
         for (unsigned int i = 0; i < info_count; i++)
         {
             // change usedGPUmemory to MB
